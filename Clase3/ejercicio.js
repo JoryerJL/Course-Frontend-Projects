e5c18781ebf6776dee8a71faac87008cacad0ce7
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2025-02-11 20:10:56 -0600

    Clase 3 Ejercicio

---
+++ Clase3/ejercicio.js
@@ -0,0 +1,7 @@
+//Tabla de multiplicar
+
+let valor = prompt('Escribe el valor de la tabla de multiplicar');
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${valor} x ${i} = ${valor * i}`);
+}
