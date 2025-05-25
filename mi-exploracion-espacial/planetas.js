81fe1463bbc924707af69d1251f1e690c7750dd1
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2025-05-24 21:21:40 -0600

    clase 15

---
+++ mi-exploracion-espacial/planetas.js
@@ -0,0 +1,29 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+  {
+    nombre: "Marte",
+    descripcion: "Conocido como el planeta rojo, tiene hielo en sus polos.",
+    descubiertoEn: "Observado desde la antigüedad"
+  },
+  {
+    nombre: "Europa",
+    descripcion: "Luna de Júpiter con un océano subterráneo que podría albergar vida.",
+    descubiertoEn: "1610"
+  },
+  {
+    nombre: "Kepler-22b",
+    descripcion: "Un exoplaneta que podría ser similar a la Tierra.",
+    descubiertoEn: "2011"
+  }
+];
+
+module.exports = planetas;
